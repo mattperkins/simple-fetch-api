63f2fdd3570e2910d9cd9f7f5249d493e5268e7d
mattperkins <github@mattperkins.com>
2018-03-17 23:45:36 +0000

    left it as simple api fetch

--- src/Form.js
+++ src/Form.js
@@ -11,8 +11,19 @@ margin: 50px 0;
 export default class Form extends React.Component {
  
  state = {
- 
+   title: '',
+   body: '' 
  }
+
+onChange = (e) => {
+ this.setState({
+  [e.target.name] : e.target.value
+})
+}
+
+//onSubmit = (e) => {
+  
+//}
 
 // MAIN COMPONENT RENDER
 render() {
@@ -23,7 +34,19 @@ return (
 <Wrapper>
 
 <p>Add Post</p>
-
+<form onSubmit={this.onSubmit}>
+ <div>
+  <label>Title: </label>
+  <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
+<br />
+ </div>
+ <div>
+  <label>Body: </label>
+<br />
+  <textarea name="body" onChange={this.onChange} value={this.state.body}/>
+ </div>
+ <button type="submit">Submit</button>
+</form>
 </Wrapper>
 
 )// end return
